947d7e5e10c5680226f44208b85fe63d799b27f0
jp-grilo <joaoplopescg@gmail.com>
2026-02-18 15:05:52 -0300

    Inicializando estrutura do projeto backend

---
+++ Backend/src/main/java/com/madeirart/appMadeirart/AppMadeirartApplication.java
@@ -0,0 +1,13 @@
+package com.madeirart.appMadeirart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMadeirartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMadeirartApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/madeirart/appMadeirart/AppMadeirartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeirart.appMadeirart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMadeirartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
