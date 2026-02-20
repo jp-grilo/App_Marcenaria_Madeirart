fcbddb3f4e27f1b3d068e61f005c4d432e206510
jp-grilo <joaoplopescg@gmail.com>
2026-02-20 09:44:36 -0300

    Remover exportações de CustosFixosList e CustosVariaveisList do componente index

--- frontend/src/views/Custos/components/index.js
+++ frontend/src/views/Custos/components/index.js
@@ -1,3 +1,1 @@
-export { default as CustosFixosList } from "./CustosFixosList";
-export { default as CustosVariaveisList } from "./CustosVariaveisList";
 export { default as CustosUnificadosList } from "./CustosUnificadosList";
